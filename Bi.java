001c917119ad3645e504c09c3f1737a42b7c2984
jisoo5531 <62212959+jisoo5531@users.noreply.github.com>
2020-09-30 17:46:40 +0900

    Add files via upload

---
+++ Bi.java
@@ -0,0 +1,18 @@
+package a;
+
+import java.util.Scanner;
+public class Bi{
+	public static void main(String[] args){
+
+		Scanner scanner = new Scanner(System.in);
+		int n=scanner.nextInt();
+
+		for(int a=0;a<=n;a++)
+			System.out.print(((a==0||a==n)?"":B(a,n))+((a==n)?"":"a^"+(n-a))+((a==0)?"":"b^"+a)+((a!=n)?" + ":""));
+		scanner.close();
+	} 
+
+	static long B(int a,int n){
+		return (a==0)?1:(a>n-a)?B(n-a,n):B(a-1,n-1)*n/a;
+	}
+}
